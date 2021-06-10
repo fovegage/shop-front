41a1f421346692b82422e21d1cf01af177ec05db
fovegage <fovegage@gmail.com>
2021-06-10 21:47:08 +0800

    1

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 let baseURL;
 switch (process.env.NODE_ENV) {
     case 'development':
-        baseURL = 'http://101.33.32.225:8002/api';
+        baseURL = 'http://101.33.32.225:8002';
         break;
     case 'prev':
         baseURL = 'http://101.33.32.225:8002/api';
